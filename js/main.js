50dc2fadecb4da848139791753c07a46499e274e
Danil <danylo_naidonov@epam.com>
2023-01-30 22:10:33 +0200

    Add filters

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 class Item {
-    constructor(item){
+    constructor(item) {
         Object.assign(this, item);
         this.like = false;
         this.orders = Math.round(Math.random() * 1000);
     }
 
-    get isAvailableForBuy(){
+    get isAvailableForBuy() {
         return this.orderInfo.inStock > 0;
     }
 
-    get absoluteImgPath(){
+    get absoluteImgPath() {
         return `img/${this.imgUrl}`;
     }
 
-    toggleLike(){
+    toggleLike() {
         return this.like = !this.like;
     }
 }
@@ -22,6 +22,18 @@ class ItemsModel {
     constructor() {
         // Create Item instances list from items list
         this.items = items.map(item => new Item(item));
+    }
+
+    get availableColors() {
+        return this.items
+            .reduce((acc, item) => [...acc, ...item.color], [])
+            .filter((item, index, arr) => arr.indexOf(item) === index);
+    }
+
+    get availableStorage() {
+        return this.items
+            .map(item => item.storage)
+            .filter((item, index, arr) => arr.indexOf(item) === index && item !== null);
     }
 
     // Get list with items based on query as substring in item name
@@ -33,7 +45,7 @@ class ItemsModel {
 
 class RenderCards {
     // Dependency injection (but it's not true)
-    constructor(itemsModel){
+    constructor(itemsModel) {
         this.cardsContainer = document.querySelector('.container'); // Container element
         this.renderCards(itemsModel.items); // Auto render cards after init page
     }
@@ -55,7 +67,7 @@ class RenderCards {
 
         const likeBtn = cardElem.querySelector('.like');
 
-        if(item.like) {
+        if (item.like) {
             likeBtn.classList.add('active');
         }
 
@@ -82,17 +94,30 @@ class RenderCards {
 class Filter {
     #itemsModel = null;
     #renderCards = null;
-    constructor(itemsModel, renderCards){
+    constructor(itemsModel, renderCards) {
         this.name = '';
         this.sort = 'default';
+        this.color = [];
+        this.storage = [];
         this.#itemsModel = itemsModel;
         this.#renderCards = renderCards;
     }
 
     setFilter(key, value) {
-        this[key] = value;
-        console.log(this)
+        if (!Array.isArray(this[key])) {
+            this[key] = value;
+            this.#findAndRerender();
+            console.log(this)
+            return;
+        }
+
+        if (this[key].includes(value)) {
+            this[key] = this[key].filter(val => val !== value);
+        } else {
+            this[key].push(value);
+        }
         this.#findAndRerender();
+        console.log(this)
     }
 
     #findAndRerender() {
@@ -101,20 +126,76 @@ class Filter {
     }
 }
 
+class RenderFilters {
+    #filter = null;
+    constructor(itemsModel, filter) {
+        this.#filter = filter;
+        this.containerElem = document.querySelector('.filters_container');
+        this.filterOptions = [
+            {
+                displayName: 'Color',
+                name: 'color',
+                options: itemsModel.availableColors,
+            },
+            {
+                displayName: 'Storage',
+                name: 'storage',
+                options: itemsModel.availableStorage,
+            },
+        ];
+
+        this.inputName = document.querySelector('.name_input');
+        this.selectSort = document.getElementById('sortFilter');
+
+        this.inputName.oninput = (event) => {
+            const { value } = event.target;
+            this.#filter.setFilter('name', value);
+        }
+
+        this.selectSort.onchange = (event) => {
+            const { value } = event.target;
+            this.#filter.setFilter('sort', value);
+        }
+
+        this.renderFilters(this.filterOptions);
+    }
+
+    static renderFilter(optionsData) {
+        const filter = document.createElement('div');
+        filter.class = 'filter_item';
+        filter.innerHTML = `<p>${optionsData.displayName}</p>`;
+        const optionsElements = optionsData.options.map(option => {
+            const filterOption = document.createElement('label');
+            filterOption.innerHTML = `<span>${option}</span>`;
+
+            const checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+            checkbox.value = option;
+            checkbox.onchange = () => {
+                this.#filter.setFilter(optionsData.name, option);
+            }
+            filterOption.appendChild(checkbox)
+
+            return filterOption;
+        })
+
+        filter.append(...optionsElements);
+
+        return filter;
+    }
+
+    renderFilters() {
+        this.containerElem.innerHTML = '';
+
+        const filtersElements = this.filterOptions
+            .map(optionData => RenderFilters.renderFilter.call(this, optionData));
+
+        this.containerElem.append(...filtersElements);
+    }
+}
+
 
 const itemsModel = new ItemsModel();
 const renderCards = new RenderCards(itemsModel);
 const filter = new Filter(itemsModel, renderCards);
-
-const inputName = document.querySelector('.name_input');
-const selectSort = document.getElementById('sortFilter');
-
-inputName.oninput = (event) => {
-    const { value } = event.target;
-    filter.setFilter('name', value);
-}
-
-selectSort.onchange = (event) => {
-    const { value } = event.target;
-    filter.setFilter('sort', value);
-}
+const renderFilters = new RenderFilters(itemsModel, filter);
